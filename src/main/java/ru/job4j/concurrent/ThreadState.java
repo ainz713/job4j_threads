f40aa0f087048d54955571692f55a8f0f1945438
ainz <ainz713@gmail.com>
2021-06-08 20:43:51 +0300

    1.1. Состояние нити.

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        state(first);
+        state(second);
+        if (first.getState() == Thread.State.TERMINATED
+                && second.getState() == Thread.State.TERMINATED) {
+            System.out.println("Работа завершена");
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    public static void state(Thread thread) {
+        System.out.println(thread.getName());
+        System.out.println(thread.getState());
+        thread.start();
+        while (thread.getState() != Thread.State.TERMINATED) {
+            System.out.println(thread.getState());
+        }
+        System.out.println(thread.getState());
+    }
+}
